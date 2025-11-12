9e36cde95e3ff9afda25f865ecad511e7cdc1cc0
Snipher Marube <sniphermarube@gmail.com>
2025-11-12 14:24:35 +0300

    django admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'completed', 'created']
+    list_filter = ('completed', 'created')
+    search_fields = ('title',)
+
+admin.site.register(Task, TaskAdmin)
