c8bcfb2dc563811980457ed6e1c5fca8c73b1a1d
Snipher Marube <sniphermarube@gmail.com>
2025-11-13 13:55:18 +0300

    Creating Views & Templates - Your First Webpage!

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'todo/task_list.html', context)
