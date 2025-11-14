30d066ac5f73ffae3afaacaf7dbc2376bdc41393
Snipher Marube <sniphermarube@gmail.com>
2025-11-14 19:26:13 +0300

    crude operations'

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title, created=timezone.now())
+        return redirect('task_list')
+    
+    tasks = Task.objects.all().order_by('-created')
     context = {
         'tasks': tasks
     }
     return render(request, 'todo/task_list.html', context)
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
+
